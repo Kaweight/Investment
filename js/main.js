41e1e841dc9acca39b6b2f54111cc1bddf0d4930
Kamil Mazelanik <kamil.mazelanik.ext@eworkgroup.com>
2023-02-28 11:43:17 +0100

    Add more styles and resolve problem with hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
 const footerYear = document.querySelector(".footer__year");
+const allMobileNavLinks = document.querySelectorAll(".nav__link");
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
 
 const handleNav = () => {
   navBtn.classList.toggle("is-active");
   navMobile.classList.toggle("nav-mobile--active");
-  console.log("click");
+  main.classList.add("main-off");
+
+  allMobileNavLinks.forEach((item) => {
+    item.addEventListener("click", () => {
+      main.classList.remove("main-off");
+      navMobile.classList.remove("nav-mobile--active");
+      navBtn.classList.remove("is-active");
+    });
+  });
 };
 
 const handleCurrentYear = () => {
